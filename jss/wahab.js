916a25374d898e876a149b035a67c16b7231cee6
Syed Abdul Wahab <syedwahab78@icloud.com>
2024-08-13 21:29:42 +0500

    Update wahab.js

--- jss/wahab.js
+++ jss/wahab.js
@@ -35,7 +35,7 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Web Developer", "Mentor", "Freelancer", "Excel Professional", "Front End Enthusiast", "PowerPoint Professional", "Sketchup Designer", "Revit Designer", "Auto CAD Designer"],
+        strings: ["Mentor", "Freelancer", "Front End Enthusiast",]
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
